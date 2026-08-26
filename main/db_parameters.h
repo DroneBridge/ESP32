e6fb1db9987fa0489885ca3f63b5580522d53cd2
Wolfgang <24637325+seeul8er@users.noreply.github.com>
2026-08-26 22:15:43 +0200

    v2.4.0 release version number update

--- main/db_parameters.h
+++ main/db_parameters.h
@@ -37,9 +37,9 @@
  */
 typedef bool (*db_param_output_callback_t)(const uint8_t *line, size_t line_length, void *context);
 
-#define DB_BUILD_VERSION 20
+#define DB_BUILD_VERSION 21
 #define DB_MAJOR_VERSION 2
-#define DB_MINOR_VERSION 3
+#define DB_MINOR_VERSION 4
 #define DB_PATCH_VERSION 0
 #define DB_MATURITY_VERSION "stable"
 #define DB_TYPE_VERSION 255 // FIRMWARE_VERSION_TYPE_OFFICIAL -> https://mavlink.io/en/messages/common.html#FIRMWARE_VERSION_TYPE
